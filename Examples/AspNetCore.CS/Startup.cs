d09142cef1c778c49b0088e1a68be5eb550473c7
GleamTech <marketing@gleamtech.com>
2018-06-25 07:35:14 -0700

    Version 4.0.8
    ## Version 4.0.8 - June 25, 2018
    
      - Fixed: "Error encoding a Flate stream" error when converting or viewing DWG with HighQualityEnabled enabled.
        DWG to PDF was fine but PDF to XPZ failed with this error.
    
      - **Improved**: Improved stability, accuracy for Spreadsheet formats.
    
      - **Improved:** Documentation.

--- Examples/AspNetCore.CS/Startup.cs
+++ Examples/AspNetCore.CS/Startup.cs
@@ -24,10 +24,6 @@ namespace GleamTech.DocumentUltimateExamples.AspNetCore.CS
             services.AddMvc();
 
             services.AddGleamTech();
-
-            var licenseFile = Hosting.ResolvePhysicalPath("~/App_Data/License.dat");
-            if (File.Exists(licenseFile))
-                DocumentUltimateConfiguration.Current.LicenseKey = File.ReadAllText(licenseFile);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +40,10 @@ namespace GleamTech.DocumentUltimateExamples.AspNetCore.CS
             }
 
             app.UseGleamTech();
+
+            var licenseFile = Hosting.ResolvePhysicalPath("~/App_Data/License.dat");
+            if (File.Exists(licenseFile))
+                DocumentUltimateConfiguration.Current.LicenseKey = File.ReadAllText(licenseFile);
 
             app.UseStaticFiles();
 
