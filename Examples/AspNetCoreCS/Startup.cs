b92be66939efd21448b33f8ada5c91242943ab3b
GleamTech <marketing@gleamtech.com>
2022-03-04 20:10:05 -0800

    Version 6.2.0
    ## Version 6.2.0 - March 5, 2021
    
      - **Changed:** .NET Framework target is changed from net40 to net461.
        So from now on, you need to have a .NET Framework 4.6.1 or above project to reference our .NET Framework DLL (not related to our .netstandard2.0 DLL).
        Minimum supported development environment version is changed from Visual Studio 2010 to Visual Studio 2012 (first to support net461 targeting pack).
        Minimum supported deployment server OS is changed from Windows Server 2003 R2 to Windows Server 2008 R2 SP1 (first to support net461 runtime).
        Minimum supported deployment client OS is changed from Windows Vista to Windows 7 SP1 (first to support net461 runtime).
    
      - **Improved:** Example projects:
    
        - Updated AspNetCoreCS project to net5.0 (Visual Studio 2019+).
    
        - Updated AspNetCoreOnNetFullCS project to net472 and ASP.NET Core 2.2.0 (Visual Studio 2017+).
    
        - Updated AspNetMvcCS and AspNetMvcVB projects to net461 and ASP.NET MVC 5.2.7 (Visual Studio 2012+).
    
        - Updated AspNetWebFormsCS and AspNetWebFormsVB projects to net461 (Visual Studio 2012+).
    
        - Fixed HintPath attributes of <Reference> elements in project files with default path "..\packages\" ("Solutiondir\packages")
          as msbuild never updates HintPath even if it restores packages via packages.config (even if we provide a NuGet.config).
          With default path, users can restore and compile the non-SDK-style projects seamlessly on their machine.
          Applies to AspNetMvcCS, AspNetMvcVB, AspNetWebFormsCS, AspNetWebFormsVB projects.

--- Examples/AspNetCoreCS/Startup.cs
+++ Examples/AspNetCoreCS/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using GleamTech.AspNet;
 using GleamTech.AspNet.Core;
 using GleamTech.DocumentUltimate;
@@ -10,21 +12,33 @@ namespace GleamTech.DocumentUltimateExamples.AspNetCoreCS
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddControllersWithViews();
 
             services.AddGleamTech();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
 
             app.UseGleamTech();
 
@@ -36,13 +50,18 @@ namespace GleamTech.DocumentUltimateExamples.AspNetCoreCS
             if (File.Exists(documentUltimateConfig))
                 DocumentUltimateConfiguration.Current.Load(documentUltimateConfig);
 
+
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapRoute(
+                endpoints.MapControllerRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
--- Examples/AspNetMvcVB/Global.asax.vb
+++ Examples/AspNetMvcVB/Global.asax.vb
@@ -1,5 +1,5 @@
-﻿' Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-' visit http://go.microsoft.com/?LinkId=9394802
+﻿Imports System.Web.Mvc
+Imports System.Web.Routing
 Imports System.IO
 Imports GleamTech.AspNet
 Imports GleamTech.DocumentUltimate
@@ -7,25 +7,9 @@ Imports GleamTech.DocumentUltimate
 Public Class MvcApplication
     Inherits System.Web.HttpApplication
 
-    Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
-        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
-
-        ' MapRoute takes the following parameters, in order:
-        ' (1) Route name
-        ' (2) URL with parameters
-        ' (3) Parameter defaults
-        routes.MapRoute(
-            "Default",
-            "{controller}/{action}/{id}",
-            New With {.controller = "Home", .action = "Index", .id = UrlParameter.Optional}
-        )
-
-    End Sub
-
-    Sub Application_Start()
+    Protected Sub Application_Start()
         AreaRegistration.RegisterAllAreas()
-
-        RegisterRoutes(RouteTable.Routes)
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
 
         Dim gleamTechConfig = Hosting.ResolvePhysicalPath("~/App_Data/GleamTech.config")
         If File.Exists(gleamTechConfig) Then
